2328bb7b17304ec4715d8282c04f44a2c0c57c26
Mohan <merranbo@gmail.com>
2025-02-10 23:09:30 -0500

    fifth

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Ankit book Shop', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ankit book Shop/i);
   expect(linkElement).toBeInTheDocument();
 });
